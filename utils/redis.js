a13485299be59ad029ad60711a7d4b232555c162
Sirothpech <christophe.ngan@gmail.com>
2024-04-01 14:20:24 +0200

    Add Redis client utility with basic operations

---
+++ utils/redis.js
@@ -0,0 +1,36 @@
+import redis from 'redis';
+import util from 'util';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error}`);
+    });
+
+    // Promisifier les méthodes de Redis
+    this.getAsync = util.promisify(this.client.get).bind(this.client);
+    this.setAsync = util.promisify(this.client.set).bind(this.client);
+    this.delAsync = util.promisify(this.client.del).bind(this.client);
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    const reply = await this.getAsync(key);
+    return reply;
+  }
+
+  async set(key, value, duration) {
+    await this.setAsync(key, value, 'EX', duration);
+  }
+
+  async del(key) {
+    await this.delAsync(key);
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
