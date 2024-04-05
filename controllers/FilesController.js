f5c58470f122fb9e220d3ab2f6e55a79d0393f9e
Sirothpech <christophe.ngan@gmail.com>
2024-04-05 13:41:10 +0200

    Refactor FilesController to streamline user and file verification, and enhance file listing with pagination

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -101,43 +101,7 @@ class FilesController {
   }
 
   static async getShow(req, res) {
-    // Verify users
-    const xToken = req.headers['x-token'];
-    if (!xToken) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-    const userId = await redisClient.get(`auth_${xToken}`);
-    if (!userId) {
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
-    const userIdString = userId.toString(); // Convert ObjectId to string
-    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userIdString) });
-    if (!user) {
-      return res.status(401).send({ error: 'Unauthorized' });
-    }
-  
-    // Verify file
-    const fileId = req.params.id;
-    if (!fileId) {
-      return res.status(404).send({ error: 'Not found' });
-    }
-    const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userIdString) });
-    if (!file) {
-      return res.status(404).send({ error: 'Not found' });
-    }
-  
-    return res.status(200).send({
-      id: file._id,
-      userId: file.userId,
-      name: file.name,
-      type: file.type,
-      isPublic: file.isPublic,
-      parentId: file.parentId,
-    });
-  }
-
-  static async getIndex(req, res) {
-    // Verify users
+    // verify user
     const xToken = req.headers['x-token'];
     if (!xToken) {
       return res.status(401).json({ error: 'Unauthorized' });
@@ -151,17 +115,74 @@ class FilesController {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    // Get files
-    const files = await dbClient.db.collection('files').find({ userId: ObjectId(userId) }).toArray();
-    return res.status(200).send(files.map((file) => ({
+    // verify file
+    const fileId = req.params.id;
+    if (!fileId) {
+      return res.status(404).send({ error: 'Not found' });
+    }
+    const document = { _id: ObjectId(fileId), userId: user._id };
+    const file = await dbClient.db.collection('files').findOne(document);
+    if (!file) {
+      return res.status(404).send({ error: 'Not found' });
+    }
+
+    // return file document
+    return res.status(200).send({
       id: file._id,
       userId: file.userId,
       name: file.name,
       type: file.type,
       isPublic: file.isPublic,
       parentId: file.parentId,
-    })));
+    });
   }
-}
+
+  static async getIndex(req, res) {
+    // verify user
+    const xToken = req.headers['x-token'];
+    if (!xToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${xToken}`);
+    if (!userId) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    // prepare to aggregate
+    const parentId = req.query.parentId || 0;
+    const page = req.query.page || 0;
+
+    let match;
+    if (parentId === 0) {
+      match = {};
+    } else {
+      match = { parentId: parentId === '0' ? Number(parentId) : ObjectId(parentId), };
+    }
+    const limit = 20;
+    const skip = page * limit;
+
+    // aggregate
+    const files = await dbClient.db.collection('files').aggregate([
+      { $match: match },
+      { $skip: skip },
+      { $limit: limit },
+    ]).toArray();
+
+    // return the list of file document
+    const filesList = files.map((file) => ({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    }));
+    return res.status(200).send(filesList);
+  }
+};
 
 export default FilesController;
