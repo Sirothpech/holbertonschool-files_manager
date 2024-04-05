ae778bbc8a34a676d6a27271e4418eb93979e6b7
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-05 12:34:13 +0200

    Add getShow and getIndex methods to FilesController for file retrieval and update routes accordingly

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -99,6 +99,69 @@ class FilesController {
       });
     }
   }
+
+  static async getShow(req, res) {
+    // Verify users
+    const xToken = req.headers['x-token'];
+    if (!xToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${xToken}`);
+    if (!userId) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    const userIdString = userId.toString(); // Convert ObjectId to string
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userIdString) });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+  
+    // Verify file
+    const fileId = req.params.id;
+    if (!fileId) {
+      return res.status(404).send({ error: 'Not found' });
+    }
+    const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId: ObjectId(userIdString) });
+    if (!file) {
+      return res.status(404).send({ error: 'Not found' });
+    }
+  
+    return res.status(200).send({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    });
+  }
+
+  static async getIndex(req, res) {
+    // Verify users
+    const xToken = req.headers['x-token'];
+    if (!xToken) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+    const userId = await redisClient.get(`auth_${xToken}`);
+    if (!userId) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+    if (!user) {
+      return res.status(401).send({ error: 'Unauthorized' });
+    }
+
+    // Get files
+    const files = await dbClient.db.collection('files').find({ userId: ObjectId(userId) }).toArray();
+    return res.status(200).send(files.map((file) => ({
+      id: file._id,
+      userId: file.userId,
+      name: file.name,
+      type: file.type,
+      isPublic: file.isPublic,
+      parentId: file.parentId,
+    })));
+  }
 }
 
 export default FilesController;
--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,7 @@ router.get('/disconnect', AuthController.getDisconnect);
 
 router.post('/files', FilesController.postUpload);
 
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
 export default router;
