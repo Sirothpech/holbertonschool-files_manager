2a20874f994ce031b838e0d61239c6ade0589b6d
Sirothpech <christophe.ngan@gmail.com>
2024-04-05 14:49:16 +0200

    Add bodyParser middleware with increased size limit and organize server setup

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
-
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
+// Configurer body-parser avec une limite de taille plus grande
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+// Utiliser le routeur
 app.use('/', router);
 
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
