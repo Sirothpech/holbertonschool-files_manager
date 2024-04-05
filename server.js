b5b6fc38ac8301a2ff706465832c39ceb14b9389
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-05 15:04:50 +0200

    Remove body-parser dependency and its configuration from server.js

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-// Configurer body-parser avec une limite de taille plus grande
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 // Utiliser le routeur
 app.use('/', router);
