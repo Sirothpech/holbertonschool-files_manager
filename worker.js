7320e922997ca0b8e9d3f69464e25933c4b6ec1d
quentinrouger <quentin_rouger@hotmail.fr>
2024-04-12 11:57:53 +0200

    Refactor worker.js to use ES6 imports and enhance file processing logic

--- worker.js
+++ worker.js
@@ -1,25 +1,23 @@
 const Bull = require('bull');
 const imageThumbnail = require('image-thumbnail');
-const dbClient = require('../holbertonschool-files_manager/utils/db'); // Import your database client
+import dbClient from './utils/db';
+import fs from 'fs';
+import { ObjectId } from 'mongodb';
 
 const fileQueue = new Bull('fileQueue');
-console.log('fileQueue');
 fileQueue.process(async (job, done)  => {
     const { fileId, userId } = job.data;
 
     // Check if fileId and userId are present in the job
     if (!fileId) {
-      console.log('hello1');
         throw new Error('Missing fileId');
     }
     if (!userId) {
-      console.log('hello2');
         throw new Error('Missing userId');
     }
     // Check if document is found in DB based on the fileId and userId
     const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId), userId });
     if (!file) {
-      console.log('hello3');
         throw new Error('File not found');
     }
 
@@ -28,13 +26,11 @@ fileQueue.process(async (job, done)  => {
     const thumbnails = [];
     const sizes = [500, 250, 100];
     for (const size of sizes) {
-      console.log('hello4');
         const thumbnail = await imageThumbnail(file.folderPath, { width: size });
         const thumbnailPath = `${file.folderPath}_${size}`;
         // Save the thumbnail to the same location with the appended width size
         fs.writeFileSync(thumbnailPath, thumbnail);
         thumbnails.push({ size, path: thumbnailPath });
-      console.log(thumbnail);
     }
 
     done();
